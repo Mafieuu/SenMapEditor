84b0d479a4366e5e0dc9e4a56e3963722390394c
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2024-12-05 12:36:07 +0000

    mbtiles / bug config systeme

---
+++ for_mbtiles/video_teste/test/widget_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MapScreen(),
+    );
+  }
+}
+class MapScreen extends StatelessWidget {
+  const MapScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Juste pour tester"),
+      ),
+      body: const Center(
+        child: Text("fin du teste"),
+      ),
+    );
+  }
+}
+

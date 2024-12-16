3de32c2cce958ab7977b13f66f5b6a3cee4880be
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2024-12-16 09:58:20 +0000

    netoyage des dossiers non a jours

--- senmapeditor/android/app/src/main/kotlin/com/example/geojson_test/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.geojson_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- senmapeditor/android/app/src/main/kotlin/com/example/geojsonmaj/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.geojsonmaj
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
